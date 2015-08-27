53ebbdb948cffdfe6594812b275beed75a7e522e
Vasil Pashov <vasilp@crytek.com>
2015-08-27 14:05:19 +0300

    Event server app done

--- DesiningAConcurrentApp/src/event.erl
+++ DesiningAConcurrentApp/src/event.erl
@@ -11,6 +11,7 @@ loop(S = #state{server=Server, to_go=[T|Next]}) ->
 		after T * 1000 ->
 				if 
 					Next =:= [] ->
+						io:format("~p Done \n", [S#state.name]), 
 						Server ! {done, S#state.name};
 					Next =/= [] ->
 						loop(S#state{to_go=Next})
@@ -21,17 +22,33 @@ normalize(N) ->
 	Limit = 49*24*60*60,
 	[N rem Limit | lists:duplicate(N div Limit, Limit)].
 
-start(EventName, Deley) ->
-	spawn(?MODULE, loop, [self(), EventName, Delay]).
+start(EventName, DateTime) ->
+	spawn(?MODULE, init, [self(), EventName, DateTime]).
 
-start_link(EventName, Delay) ->
-	spawn_link(?MODULE, loop, [self(), EventName, Delay]).
+start_link(EventName, DateTime) ->
+	spawn_link(?MODULE, init, [self(), EventName, DateTime]).
 
-init(Server, EventName, Delay) ->
-	loop(#state{server=Server(),
+init(Server, EventName, DateTime) ->
+	loop(#state{server=Server,
 				name=EventName,
-				to_go=normalize(Delay)}).
+				to_go=time_to_go(DateTime)}).
 
 cancel(Pid) ->
 	Ref = erlang:monitor(process, Pid),
 	Pid ! {self(), Ref, cancel},
+	receive
+		{Ref, ok} ->
+			erlang:demonitor(Ref, [flush]),
+			ok;
+		{'DOWN', Ref, process, Pid, _Reason} ->
+			ok
+	end. 
+
+time_to_go(TimeOut={{_,_,_}, {_,_,_}}) ->
+	Now = calendar:local_time(),
+	ToGo = calendar:datetime_to_gregorian_seconds(TimeOut) - 
+			calendar:datetime_to_gregorian_seconds(Now),
+	Secs = if ToGo > 0 -> ToGo;
+				ToGo =< 0 -> 0
+			end,
+	normalize(Secs).
